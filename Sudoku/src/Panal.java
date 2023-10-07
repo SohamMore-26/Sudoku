6d40ff93f8646bd5e89c2d64db15180011f08926
soham <sohammore081@gmail.com>
2023-10-07 20:58:36 +0530

    ...

---
+++ Sudoku/src/Panal.java
@@ -0,0 +1,262 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Panal extends javax.swing.JPanel {
+
+    Sudoku game;
+    private JButton nbtn = new JButton("new game");
+    private static JTextField[][] boxes;
+    private JPanel[][] paneles;
+    private JPanel center, bPanel;
+    private JButton nBtn, cBtn, eBtn, hardBtn, midBtn, easyBtn, slove;
+    private int[][] temp = new int[9][9];
+    private int[][] grid = new int[9][9];
+
+
+    public JTextField newtextfield() {
+        JTextField j = new JTextField("");
+        j.setBorder(BorderFactory.createLineBorder(Color.lightGray));
+        j.setFont(new Font(Font.DIALOG, Font.PLAIN, 25));
+        j.setHorizontalAlignment(JTextField.CENTER);
+        /*-------------------mouse lisner----------------*/
+        j.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                if (j.isEditable()) {
+                    ((JTextField) e.getSource()).setBorder(BorderFactory.createLineBorder(Color.decode("#f6ea80")));
+                    ((JTextField) e.getSource()).setBackground(Color.decode("#f6ea80"));
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                if (j.isEditable()) {
+                    ((JTextField) e.getSource()).setBorder(BorderFactory.createLineBorder(Color.lightGray));
+                    ((JTextField) e.getSource()).setBackground(Color.white);
+                }
+            }
+        });
+        /*------------------------------------------------*/
+
+        j.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (j.isEditable()) {
+                    ((JTextField) e.getSource()).setForeground(Color.decode("#0c4"));
+                } else {
+                    ((JTextField) e.getSource()).setForeground(Color.black);
+                }
+            }
+        });
+        return j;
+    }
+    
+    public Panal() {
+        initComponents();
+        /*------------------------main panal  -------------------------------------*/
+        center = new JPanel();  //main panel
+        center.setLayout(new GridLayout(3, 3));     //grid for 3*3 
+        center.setBackground(Color.BLACK);
+        setLayout(new BorderLayout());
+        add(center);  //add main panel to frame 
+
+        boxes = new JTextField[9][9];
+        paneles = new JPanel[3][3];
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                paneles[i][j] = new JPanel();
+                paneles[i][j].setBorder(BorderFactory.createLineBorder(Color.black));
+                paneles[i][j].setLayout(new GridLayout(3, 3));
+                center.add(paneles[i][j]);
+            }
+        }
+        /*------------------------text fildes in boxes-------------------------------------*/
+
+        for (int n = 0; n < 9; n++) {
+            for (int i = 0; i < 9; i++) {
+                boxes[n][i] = newtextfield();
+                int fm = (n + 1) / 3;
+                if ((n + 1) % 3 > 0) {
+                    fm++;
+                }
+                int cm = (i + 1) / 3;
+                if ((i + 1) % 3 > 0) {
+                    cm++;
+                }
+                paneles[fm - 1][cm - 1].add(boxes[n][i]);   //add box to panel 
+            }
+        }
+        /*------------------------panal for buttons -------------------------------------*/
+
+        bPanel = new JPanel();
+        bPanel.setBackground(Color.decode("#AABFFF"));
+        bPanel.setBorder(BorderFactory.createLineBorder(Color.black, 6, true));
+        bPanel.setLayout(new GridLayout(3, 2, 2, 10));
+
+
+        /*------------------------new game button -------------------------------------*/
+        nBtn = new JButton("New Game");
+        nbtn.setSize(20, 50);
+        nBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restgame();
+                Sudoku.newGame();
+
+            }
+        });
+
+        /*------------------------check game button -------------------------------------*/
+        cBtn = new JButton("Check Game");
+
+        cBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        if (!boxes[i][j].isEditable()) {
+                            continue;
+                        } else if (boxes[i][j].getText().equals(String.valueOf(grid[i][j]))) {
+                            boxes[i][j].setBackground(Color.decode("#C0DCD9"));
+                        } else if (boxes[i][j].getText().isEmpty()) {
+                            boxes[i][j].setBackground(Color.WHITE);
+                            continue;
+                        } else {
+                            boxes[i][j].setBackground(Color.red);
+                        }
+                    }
+                }
+            }
+        });
+
+        /*------------------------Hard button -------------------------------------*/
+        hardBtn = new JButton("Hard");
+
+        hardBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restgame();
+                Sudoku.setlevel(4);
+                Sudoku.newGame();
+            }
+        });
+        /*------------------------medium button -------------------------------------*/
+        midBtn = new JButton("Medium");
+
+        midBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restgame();
+                Sudoku.setlevel(3);
+                Sudoku.newGame();
+
+            }
+        });
+        /*------------------------easy button -------------------------------------*/
+        easyBtn = new JButton("Easy");
+
+        easyBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restgame();
+                Sudoku.setlevel(2);
+                Sudoku.newGame();
+            }
+        });
+        /*------------------------panal for new Hard button -------------------------------------*/
+        slove = new JButton("Solve");
+
+        slove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                    for (int i = 0; i < 9; i++) {
+                        for (int j = 0; j < 9; j++) {
+                            boxes[i][j].setText(String.valueOf(grid[i][j]));
+                        }
+                    }
+            }
+        });
+
+        /*------------------------add button panal and butons to frame and panel -------------------------------------*/
+        bPanel.add(hardBtn);   //add new game button to 
+        bPanel.add(midBtn);
+        bPanel.add(easyBtn);
+        bPanel.add(nBtn);   //add new game button to 
+        bPanel.add(cBtn);
+        bPanel.add(slove);
+
+        add(bPanel, "South");   //add button panel to frame 
+
+    }
+
+    public void setarray(int[][] grid, int[][] temp) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                this.temp[i][j] = temp[i][j];
+                this.grid[i][j] = grid[i][j];
+            }
+        }
+    }
+
+    public void setTextLable() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (this.temp[i][j] != 0) {
+                    boxes[i][j].setText(String.valueOf(this.temp[i][j]));
+                    boxes[i][j].setEditable(false);
+                    boxes[i][j].setBackground(Color.decode("#C0DCC0"));
+                } else {
+                    boxes[i][j].setText("");
+                }
+            }
+        }
+    }
+
+    public static void restgame() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                boxes[i][j].setForeground(Color.black);
+                boxes[i][j].setEditable(true);
+                boxes[i][j].setBackground(Color.WHITE);
+            }
+        }
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setLayout(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
